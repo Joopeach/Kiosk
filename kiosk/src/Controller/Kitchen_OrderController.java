39d95a8ea85c32d562c864cd433367794ad209eb
joopeach <joopeach@kakao.com>
2021-07-09 11:26:01 +0900

    수정

---
+++ kiosk/src/Controller/Kitchen_OrderController.java
@@ -0,0 +1,48 @@
+package Controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class Kitchen_OrderController implements Initializable { // 직원이 주방에서 보는 창
+
+	@FXML
+    private TableView<?> tableview;
+
+    @FXML
+    private Button btnback;
+
+    @FXML
+    private Label lblorder;
+
+    @FXML
+    void back(ActionEvent event) {
+
+    }
+
+    @FXML
+    void order(MouseEvent event) {
+
+    }
+
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+}
